6761cdde4d72cd8bd7c8a261555b9fe0e81af12e
tallzilla <bill@billheil.com>
2020-10-01 15:18:10 -0700

    argh mess

--- docs/src.fcdf4644.js
+++ docs/src.fcdf4644.js
@@ -117,7 +117,7 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   }
 
   return newRequire;
-})({"../../../../AppData/Roaming/npm/node_modules/parcel-bundler/node_modules/process/browser.js":[function(require,module,exports) {
+})({"MyHX":[function(require,module,exports) {
 
 // shim for using process in browser
 var process = module.exports = {}; // cached from whatever global is present so that test runners that stub it
@@ -326,7 +326,7 @@ process.chdir = function (dir) {
 process.umask = function () {
   return 0;
 };
-},{}],"node_modules/whatwg-fetch/fetch.js":[function(require,module,exports) {
+},{}],"MCp7":[function(require,module,exports) {
 
 "use strict";
 
@@ -942,7 +942,7 @@ if (!global.fetch) {
   global.Request = Request;
   global.Response = Response;
 }
-},{}],"node_modules/isomorphic-fetch/fetch-npm-browserify.js":[function(require,module,exports) {
+},{}],"kQO1":[function(require,module,exports) {
 // the whatwg-fetch polyfill installs the fetch() function
 // on the global object (window or self)
 //
@@ -950,7 +950,7 @@ if (!global.fetch) {
 require('whatwg-fetch');
 module.exports = self.fetch.bind(self);
 
-},{"whatwg-fetch":"node_modules/whatwg-fetch/fetch.js"}],"node_modules/fetch-retry/index.js":[function(require,module,exports) {
+},{"whatwg-fetch":"MCp7"}],"UUcy":[function(require,module,exports) {
 'use strict';
 
 module.exports = function (fetch, defaults) {
@@ -1070,7 +1070,7 @@ function ArgumentError(message) {
   this.message = message;
 }
 
-},{}],"index.js":[function(require,module,exports) {
+},{}],"Focm":[function(require,module,exports) {
 
 // Get the built-in Node process module (lets me get env variables)
 var process = require('process'); //from https://www.npmjs.com/package/fetch-retry
@@ -1084,9 +1084,9 @@ var fetch = require('fetch-retry')(originalFetch); // Create the script tag, set
 var script = document.createElement('script');
 var api_key = undefined;
 script.src = 'https://maps.googleapis.com/maps/api/js?key=' + api_key + '&callback=initMap';
-console.log("Here's the src " + script.src);
-console.log("Here's your doggo: " + undefined);
-console.log("Here's your doggo: " + undefined);
+console.log("Here's the url we're getting " + script.src);
+console.log("Secrets try:" + undefined);
+console.log("Secrets try:" + undefined);
 script.defer = true;
 script.async = true;
 
@@ -1144,24 +1144,7 @@ window.initMap = function () {
   for (var id in purpleDeviceIds) {
     _loop(id);
   }
-}; //from: https://developers.google.com/maps/documentation/javascript/combining-data#maps_combining_data-javascript
-
-/** Loads the state boundary polygons from a GeoJSON source. */
-
-
-function loadMapShapes() {
-  // load US state outline polygons from a GeoJson file
-  map.data.loadGeoJson("https://storage.googleapis.com/mapsdevsite/json/states.js", {
-    idPropertyName: "STATE"
-  }); // wait for the request to complete by listening for the first feature to be
-  // added
-  // google.maps.event.addListenerOnce(map.data, "addfeature", () => {
-  //   google.maps.event.trigger(
-  //     document.getElementById("census-variable"),
-  //     "change"
-  //   );
-  // });
-} //here's a reference on calculating AQI
+}; //here's a reference on calculating AQI
 //https://github.com/dazimmermann/PurpleAir/blob/master/PurpleAir/Program.cs
 
 
@@ -1197,209 +1180,5 @@ function pm25ToAQI(pm25) {
 
 
 document.head.appendChild(script);
-},{"process":"../../../../AppData/Roaming/npm/node_modules/parcel-bundler/node_modules/process/browser.js","isomorphic-fetch":"node_modules/isomorphic-fetch/fetch-npm-browserify.js","fetch-retry":"node_modules/fetch-retry/index.js"}],"../../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
-var global = arguments[3];
-var OVERLAY_ID = '__parcel__error__overlay__';
-var OldModule = module.bundle.Module;
-
-function Module(moduleName) {
-  OldModule.call(this, moduleName);
-  this.hot = {
-    data: module.bundle.hotData,
-    _acceptCallbacks: [],
-    _disposeCallbacks: [],
-    accept: function (fn) {
-      this._acceptCallbacks.push(fn || function () {});
-    },
-    dispose: function (fn) {
-      this._disposeCallbacks.push(fn);
-    }
-  };
-  module.bundle.hotData = null;
-}
-
-module.bundle.Module = Module;
-var checkedAssets, assetsToAccept;
-var parent = module.bundle.parent;
-
-if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
-  var hostname = "" || location.hostname;
-  var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55040" + '/');
-
-  ws.onmessage = function (event) {
-    checkedAssets = {};
-    assetsToAccept = [];
-    var data = JSON.parse(event.data);
-
-    if (data.type === 'update') {
-      var handled = false;
-      data.assets.forEach(function (asset) {
-        if (!asset.isNew) {
-          var didAccept = hmrAcceptCheck(global.parcelRequire, asset.id);
-
-          if (didAccept) {
-            handled = true;
-          }
-        }
-      }); // Enable HMR for CSS by default.
-
-      handled = handled || data.assets.every(function (asset) {
-        return asset.type === 'css' && asset.generated.js;
-      });
-
-      if (handled) {
-        console.clear();
-        data.assets.forEach(function (asset) {
-          hmrApply(global.parcelRequire, asset);
-        });
-        assetsToAccept.forEach(function (v) {
-          hmrAcceptRun(v[0], v[1]);
-        });
-      } else if (location.reload) {
-        // `location` global exists in a web worker context but lacks `.reload()` function.
-        location.reload();
-      }
-    }
-
-    if (data.type === 'reload') {
-      ws.close();
-
-      ws.onclose = function () {
-        location.reload();
-      };
-    }
-
-    if (data.type === 'error-resolved') {
-      console.log('[parcel] ✨ Error resolved');
-      removeErrorOverlay();
-    }
-
-    if (data.type === 'error') {
-      console.error('[parcel] 🚨  ' + data.error.message + '\n' + data.error.stack);
-      removeErrorOverlay();
-      var overlay = createErrorOverlay(data);
-      document.body.appendChild(overlay);
-    }
-  };
-}
-
-function removeErrorOverlay() {
-  var overlay = document.getElementById(OVERLAY_ID);
-
-  if (overlay) {
-    overlay.remove();
-  }
-}
-
-function createErrorOverlay(data) {
-  var overlay = document.createElement('div');
-  overlay.id = OVERLAY_ID; // html encode message and stack trace
-
-  var message = document.createElement('div');
-  var stackTrace = document.createElement('pre');
-  message.innerText = data.error.message;
-  stackTrace.innerText = data.error.stack;
-  overlay.innerHTML = '<div style="background: black; font-size: 16px; color: white; position: fixed; height: 100%; width: 100%; top: 0px; left: 0px; padding: 30px; opacity: 0.85; font-family: Menlo, Consolas, monospace; z-index: 9999;">' + '<span style="background: red; padding: 2px 4px; border-radius: 2px;">ERROR</span>' + '<span style="top: 2px; margin-left: 5px; position: relative;">🚨</span>' + '<div style="font-size: 18px; font-weight: bold; margin-top: 20px;">' + message.innerHTML + '</div>' + '<pre>' + stackTrace.innerHTML + '</pre>' + '</div>';
-  return overlay;
-}
-
-function getParents(bundle, id) {
-  var modules = bundle.modules;
-
-  if (!modules) {
-    return [];
-  }
-
-  var parents = [];
-  var k, d, dep;
-
-  for (k in modules) {
-    for (d in modules[k][1]) {
-      dep = modules[k][1][d];
-
-      if (dep === id || Array.isArray(dep) && dep[dep.length - 1] === id) {
-        parents.push(k);
-      }
-    }
-  }
-
-  if (bundle.parent) {
-    parents = parents.concat(getParents(bundle.parent, id));
-  }
-
-  return parents;
-}
-
-function hmrApply(bundle, asset) {
-  var modules = bundle.modules;
-
-  if (!modules) {
-    return;
-  }
-
-  if (modules[asset.id] || !bundle.parent) {
-    var fn = new Function('require', 'module', 'exports', asset.generated.js);
-    asset.isNew = !modules[asset.id];
-    modules[asset.id] = [fn, asset.deps];
-  } else if (bundle.parent) {
-    hmrApply(bundle.parent, asset);
-  }
-}
-
-function hmrAcceptCheck(bundle, id) {
-  var modules = bundle.modules;
-
-  if (!modules) {
-    return;
-  }
-
-  if (!modules[id] && bundle.parent) {
-    return hmrAcceptCheck(bundle.parent, id);
-  }
-
-  if (checkedAssets[id]) {
-    return;
-  }
-
-  checkedAssets[id] = true;
-  var cached = bundle.cache[id];
-  assetsToAccept.push([bundle, id]);
-
-  if (cached && cached.hot && cached.hot._acceptCallbacks.length) {
-    return true;
-  }
-
-  return getParents(global.parcelRequire, id).some(function (id) {
-    return hmrAcceptCheck(global.parcelRequire, id);
-  });
-}
-
-function hmrAcceptRun(bundle, id) {
-  var cached = bundle.cache[id];
-  bundle.hotData = {};
-
-  if (cached) {
-    cached.hot.data = bundle.hotData;
-  }
-
-  if (cached && cached.hot && cached.hot._disposeCallbacks.length) {
-    cached.hot._disposeCallbacks.forEach(function (cb) {
-      cb(bundle.hotData);
-    });
-  }
-
-  delete bundle.cache[id];
-  bundle(id);
-  cached = bundle.cache[id];
-
-  if (cached && cached.hot && cached.hot._acceptCallbacks.length) {
-    cached.hot._acceptCallbacks.forEach(function (cb) {
-      cb();
-    });
-
-    return true;
-  }
-}
-},{}]},{},["../../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js","index.js"], null)
-//# sourceMappingURL=/src.e31bb0bc.js.map
+},{"process":"MyHX","isomorphic-fetch":"kQO1","fetch-retry":"UUcy"}]},{},["Focm"], null)
+//# sourceMappingURL=https://billheil.com/purpleair-map/src.fcdf4644.js.map
